a8a86a52375b2d12e8c61ece3784c3808a0579b4
zyhazwraith <zyhazwraith@outlook.com>
2017-06-04 21:46:38 +0800

    proto.tcpWrite

--- tcpserver.go
+++ tcpserver.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"fmt"
+	//	"fmt"
 	"log"
 	"net"
 	"time"
 )
 
 func main() {
-	tcpAddr := net.ResolveTCPAddr("tcp", "127.0.0.1:8989")
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8989")
 	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer tcpListener.Close()
 	for {
 		tcpConn, err := tcpListener.AcceptTCP()
 		if err != nil {
 			log.Fatal(err)
 		}
-		handleConn(tcpConn)
+		log.Print("handle ", tcpConn.RemoteAddr().String())
+		go handleConn(tcpConn)
 	}
 }
 
@@ -34,24 +38,32 @@ func handleConn(conn *net.TCPConn) {
 		go heartBeat(conn, messager)
 
 		go gravelChan(data, messager)
+		log.Print("received ", data)
+		log.Print("length: ", n)
 	}
 }
 
-func heartBeat(conn *net.Conn, readchan chan byte) {
-	select {
-	case fk := <-readchan:
-		log.Print("received ", fk)
-		conn.SetDeadline(time.Now().Add(time.Duration(5) * time.Second))
-		log.Print("xu zhu le")
-	case <-time.After(time.Second * 5):
-		log.Print("nothing receied in 5 secs")
-		conn.Close()
+func heartBeat(conn *net.TCPConn, readchan chan byte) {
+	for ok := 1; ok < 20; ok++ {
+		select {
+		case fk := <-readchan:
+			log.Print("received ", fk)
+			conn.SetDeadline(time.Now().Add(time.Duration(5) * time.Second))
+			log.Print("xu zhu le")
+		case <-time.After(time.Second * 5):
+			log.Print("nothing receied in 5 secs")
+			conn.Close()
+			//			ok = false
+		}
 	}
 }
 
 func gravelChan(data []byte, messager chan byte) {
+	//	log.Print("data len ", len(data))
 	for _, v := range data {
 		messager <- v
+		log.Print("push")
 	}
-	close(messager)
+	// if close channel in advance, fk will read `` data
+	//	close(messager)
 }
